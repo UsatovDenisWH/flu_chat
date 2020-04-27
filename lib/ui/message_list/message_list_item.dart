dd08d620929a366f9d0a46b3eb1fa41b392a59b7
Veselchak-U <usatov-dv@ya.ru>
2020-04-27 23:42:42 +0300

    UI message list modifications

---
+++ lib/ui/message_list/message_list_item.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class MessageListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+      height: 100.0,
+      color: Colors.lightBlueAccent,
+    );
+
+}
--- lib/ui/message_list/message_list_route.dart
+++ lib/ui/message_list/message_list_route.dart
@@ -1,4 +1,5 @@
 import 'package:fluchat/models/chat_item.dart';
+import 'package:fluchat/ui/message_list/message_list_item.dart';
 import 'package:flutter/material.dart';
 
 class MessageListRoute extends StatelessWidget {
@@ -10,57 +11,109 @@ class MessageListRoute extends StatelessWidget {
     if (chatItem.isOnline) {
       titleSubscription = "В сети";
     } else {
-      // TODO change "chatItem.lastMessageDate" on "user.lastSeen"
+      // TODO replace "chatItem.lastMessageDate" with "user.lastSeen"
       titleSubscription = "был: ${chatItem.lastMessageDate}";
     }
 
-    return Scaffold(
-      appBar: AppBar(
-          titleSpacing: 0.0,
-          title: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(children: <Widget>[
-              if (chatItem.avatar == "")
-                CircleAvatar(
-                  radius: 22.0,
-                  backgroundColor: Colors.blue[300],
-                  foregroundColor: Colors.white,
-                  child:
-                      Text(chatItem.initials, style: TextStyle(fontSize: 16.0)),
-                ),
-              if (chatItem.avatar != "")
-                CircleAvatar(
-                  backgroundColor: Colors.blue[300],
-                  radius: 22.0,
-                  // TODO catch loading error
-                  backgroundImage: NetworkImage(chatItem.avatar),
-                ),
-              SizedBox(width: 8.0),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      chatItem.title,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 16.0),
-                    ),
-                    SizedBox(height: 4.0),
-                    Text(
-                      titleSubscription,
-                      style: TextStyle(
-                          fontWeight: FontWeight.normal, fontSize: 14.0),
-                    ),
-                  ],
-                ),
+    AppBar _appBar = AppBar(
+        titleSpacing: 0.0,
+        title: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(children: <Widget>[
+            if (chatItem.avatar == "")
+              CircleAvatar(
+                radius: 22.0,
+                backgroundColor: Colors.blue[300],
+                foregroundColor: Colors.white,
+                child:
+                    Text(chatItem.initials, style: TextStyle(fontSize: 16.0)),
               ),
-            ]),
-          ),
-          actions: <Widget>[
-            PopupMenuButton(itemBuilder: (BuildContext context) {}),
+            if (chatItem.avatar != "")
+              CircleAvatar(
+                backgroundColor: Colors.blue[300],
+                radius: 22.0,
+                // TODO catch loading error
+                backgroundImage: NetworkImage(chatItem.avatar),
+              ),
+            SizedBox(width: 8.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    chatItem.title,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    titleSubscription,
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal, fontSize: 14.0),
+                  ),
+                ],
+              ),
+            ),
           ]),
-      body: Stack(
+        ),
+        actions: <Widget>[
+          PopupMenuButton(itemBuilder: (BuildContext context) {}),
+        ]);
+
+    Widget _body = SafeArea(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.separated(
+                padding: EdgeInsets.all(0),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) =>
+                    MessageListItem(),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(height: 1)),
+          ),
+          Container(
+            height: 48.0,
+            color: Colors.white70,
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.tag_faces),
+                  onPressed: () {
+                    print("emodzi pressed");
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Сообщение"),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.attach_file),
+                  onPressed: () {
+                    print("emodzi pressed");
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.camera_alt),
+                  onPressed: () {
+                    print("emodzi pressed");
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: _appBar,
+      body: _body,
+      /*Stack(
         children: <Widget>[
           Column(
             children: <Widget>[
@@ -77,7 +130,7 @@ class MessageListRoute extends StatelessWidget {
             ],
           ),
         ],
-      ),
+      ),*/
     );
   }
 }
